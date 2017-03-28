5786d558b8e7ab8755a49f5d5bde86217abbdb72
Catarina <catarina.edp@gmail.com>
2017-03-28 15:18:43 -0300

    Modelo de Banco de Dados + squeezebd.sql

--- squeezesql.sql
+++ squeezesql.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win32 (AMD64)
+CREATE DATABASE  IF NOT EXISTS `squeezebd` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `squeezebd`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
 --
 -- Host: 127.0.0.1    Database: squeezebd
 -- ------------------------------------------------------
--- Server version	5.7.16-log
+-- Server version	5.6.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,7 +30,7 @@ CREATE TABLE `album` (
   `estudio` varchar(45) DEFAULT NULL,
   `dtlancamento` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +39,7 @@ CREATE TABLE `album` (
 
 LOCK TABLES `album` WRITE;
 /*!40000 ALTER TABLE `album` DISABLE KEYS */;
-INSERT INTO `album` VALUES (1,'aasdsa','asdasd','sadasd'),(2,'asdasd','sadasd','asdasdd'),(3,'dddd','dddd','ddd'),(4,'aaaa','aaaa','quinta-feira, 9 de março de 2017'),(5,'aaa','aaa','quinta-feira, 9 de março de 2017'),(6,'aaaa','aaaa','sexta-feira, 10 de março de 2017');
+INSERT INTO `album` VALUES (1,'Green Light','Som Livre','terça-feira, 28 de março de 2017'),(2,'Next to Me','Som Livre','terça-feira, 28 de março de 2017');
 /*!40000 ALTER TABLE `album` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +67,7 @@ CREATE TABLE `albumartista` (
 
 LOCK TABLES `albumartista` WRITE;
 /*!40000 ALTER TABLE `albumartista` DISABLE KEYS */;
-INSERT INTO `albumartista` VALUES (2014120312,6);
+INSERT INTO `albumartista` VALUES (2014120312,1),(2014120313,2);
 /*!40000 ALTER TABLE `albumartista` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +86,7 @@ CREATE TABLE `artista` (
   PRIMARY KEY (`id`),
   KEY `fk_artistas_carreiraMusical1_idx` (`tipocarreira`),
   CONSTRAINT `fk_artistas_carreiraMusical1` FOREIGN KEY (`tipocarreira`) REFERENCES `tipocarreira` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2014120318 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2014120314 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +95,7 @@ CREATE TABLE `artista` (
 
 LOCK TABLES `artista` WRITE;
 /*!40000 ALTER TABLE `artista` DISABLE KEYS */;
-INSERT INTO `artista` VALUES (2014120312,'Lorde',1,'quinta-feira, 9 de março de 2017'),(2014120313,'Phantogram',1,'quinta-feira, 9 de março de 2017'),(2014120314,'Sigrid',1,'quinta-feira, 9 de março de 2017'),(2014120315,'aaaaa',1,'quinta-feira, 9 de março de 2017'),(2014120316,'fffff',1,'quinta-feira, 9 de março de 2017'),(2014120317,'gggg',1,'quinta-feira, 9 de março de 2017');
+INSERT INTO `artista` VALUES (2014120312,'Lorde',1,'terça-feira, 28 de março de 2017'),(2014120313,'Emeli Sandé',1,'terça-feira, 28 de março de 2017');
 /*!40000 ALTER TABLE `artista` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,14 +134,14 @@ DROP TABLE IF EXISTS `faixa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `faixa` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) DEFAULT NULL,
   `duracao` varchar(45) DEFAULT NULL,
   `idalbum` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_faixas_albuns1_idx` (`idalbum`),
   CONSTRAINT `fk_faixas_albuns1` FOREIGN KEY (`idalbum`) REFERENCES `album` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,7 +150,7 @@ CREATE TABLE `faixa` (
 
 LOCK TABLES `faixa` WRITE;
 /*!40000 ALTER TABLE `faixa` DISABLE KEYS */;
-INSERT INTO `faixa` VALUES (1,'zzz','zzz',6),(2,'sssss','ssss',6);
+INSERT INTO `faixa` VALUES (1,'Liability','4:14',1),(2,'Green Light','4:15',1);
 /*!40000 ALTER TABLE `faixa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,6 +178,7 @@ CREATE TABLE `generoartista` (
 
 LOCK TABLES `generoartista` WRITE;
 /*!40000 ALTER TABLE `generoartista` DISABLE KEYS */;
+INSERT INTO `generoartista` VALUES (2014120312,2),(2014120312,3),(2014120313,5),(2014120313,6);
 /*!40000 ALTER TABLE `generoartista` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -188,9 +191,9 @@ DROP TABLE IF EXISTS `tipocarreira`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tipocarreira` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) DEFAULT NULL,
+  `nome` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -199,7 +202,7 @@ CREATE TABLE `tipocarreira` (
 
 LOCK TABLES `tipocarreira` WRITE;
 /*!40000 ALTER TABLE `tipocarreira` DISABLE KEYS */;
-INSERT INTO `tipocarreira` VALUES (1,'Solo');
+INSERT INTO `tipocarreira` VALUES (1,'Solo'),(2,'Dupla'),(3,'Trio'),(4,'Banda');
 /*!40000 ALTER TABLE `tipocarreira` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -211,10 +214,10 @@ DROP TABLE IF EXISTS `tipogenero`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tipogenero` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,7 +226,7 @@ CREATE TABLE `tipogenero` (
 
 LOCK TABLES `tipogenero` WRITE;
 /*!40000 ALTER TABLE `tipogenero` DISABLE KEYS */;
-INSERT INTO `tipogenero` VALUES (1,'Pop'),(2,'Indie');
+INSERT INTO `tipogenero` VALUES (1,'MPB'),(2,'Pop'),(3,'Indie'),(4,'Rock'),(5,'Soul'),(6,'Jazz');
 /*!40000 ALTER TABLE `tipogenero` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -241,7 +244,7 @@ CREATE TABLE `usuario` (
   `senha` varchar(8) DEFAULT NULL,
   `tipo` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2014120308 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -250,7 +253,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'catarina','catarina','123',1);
+INSERT INTO `usuario` VALUES (1,'catarina','catarina','123',1),(2014120307,'thaissa','thaissa','123',0);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -263,4 +266,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-10 10:48:47
+-- Dump completed on 2017-03-28 15:17:54
